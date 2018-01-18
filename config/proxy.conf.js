777dc869b788d38e06d65c33d8838de353cb0125
Alex Sorafumo <alex@yuion.net>
2018-01-18 23:25:33 +1100

    Add proxy config. Add optimisation tasks

---
+++ config/proxy.conf.js
@@ -0,0 +1,14 @@
+const PROXY_CONFIG = [
+    {
+        context: [
+            "/control",
+            "/auth",
+            "/app_api",
+        ],
+        target: "http://localhost:8080",
+        secure: false,
+        changeOrigin: true
+    }
+]
+
+module.exports = PROXY_CONFIG;
